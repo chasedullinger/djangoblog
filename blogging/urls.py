b67c402ce249ad19aecfd089c244da4ff9624d9d
Chase Dullinger <chase.dullinger@gmail.com>
2021-03-23 19:29:43 -0700

    Enabling forms and rss feed

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import stub_view, list_view, detail_view
+from blogging.views import stub_view, list_view, detail_view, form_view
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
+    path('posts/form', form_view, name="blog_form"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from blogging.forms import BlogForm
 
 
 def stub_view(request, *args, **kwargs):
@@ -32,3 +33,20 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+
+def form_view(request):
+
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            # model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('/')
+
+    else:
+
+        form = BlogForm()
+
+        return render(request, "blogging/form.html", {'form': form})
